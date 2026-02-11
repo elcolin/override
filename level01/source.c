df07da20b908bd572dd05a8f1ab303122ce7acea
elcolin <61796343+elcolin@users.noreply.github.com>
2026-02-11 13:04:10 +0100

    Update source.c level01
    Rajouts de fonctions necessaire a la comprehension de l'exercice.

--- level01/source.c
+++ level01/source.c
@@ -1,57 +1,79 @@
+int32_t verify_user_name()
+{
+    int32_t __saved_esi;
+    bool c = &__saved_esi < 0x10;
+    bool z = &__saved_esi == 0x10;
+    puts("verifying username....\n");
+    int32_t i = 7;
+    char* esi = &a_user_name;
+    char* edi = "dat_wil";
+    
+    while (i)
+    {
+        char temp1_1 = *esi;
+        char temp2_1 = *edi;
+        c = temp1_1 < temp2_1;
+        z = temp1_1 == temp2_1;
+        esi = &esi[1];
+        edi = &edi[1];
+        i -= 1;
+        
+        if (!z)
+            break;
+    }
+    
+    return (!z && !c) - c;
+}
+
+int32_t verify_user_pass(char* arg1)
+{
+    int32_t i = 5;
+    char* esi = arg1;
+    char* edi = "admin";
+    bool c;
+    bool z;
+    
+    while (i)
+    {
+        char temp0_1 = *esi;
+        char temp1_1 = *edi;
+        c = temp0_1 < temp1_1;
+        z = temp0_1 == temp1_1;
+        esi = &esi[1];
+        edi = &edi[1];
+        i -= 1;
+        
+        if (!z)
+            break;
+    }
+    
+    char* edx;
+    edx = !z && !c;
+    return edx - c;
+}
+
 int32_t main(int32_t argc, char** argv, char** envp)
 {
-    int32_t argc_1 = argc;
-    char** argv_1 = argv;
-    char var_78[0x64];
-    *__builtin_memset(&var_78, 0, 0x60) = 0;
-    char buf_1[0x30];
-    *__builtin_memset(&buf_1, 0, 0x28) = 0;
-    char buf[0x40];
-    *__builtin_memset(&buf, 0, 0x60) = 0;
-    int64_t var_10 = 0;
+    void buf;
+    __builtin_memset(&buf, 0, 0x40);
     int32_t var_14 = 0;
-    FILE* fp = fopen("/home/users/level03/.pass", "r");
+    puts("********* ADMIN LOGIN PROMPT *********");
+    printf("Enter Username: ");
+    fgets(&a_user_name, 0x100, stdin);
     
-    if (!fp)
+    if (verify_user_name())
     {
-        fwrite("ERROR: failed to open password file\n", 1, 0x24, stderr);
-        exit(1);
-        /* no return */
+        puts("nope, incorrect username...\n");
+        return 1;
     }
     
-    int32_t rax = fread(&buf_1, 1, 0x29, fp);
-    buf_1[strcspn(&buf_1, "\n")] = 0;
+    puts("Enter Password: ");
+    fgets(&buf, 0x64, stdin);
+    int32_t eax_2 = verify_user_pass(&buf);
     
-    if (rax != 0x29)
-    {
-        fwrite("ERROR: failed to read password file\n", 1, 0x24, stderr);
-        fwrite("ERROR: failed to read password file\n", 1, 0x24, stderr);
-        exit(1);
-        /* no return */
-    }
-    
-    fclose(fp);
-    puts("===== [ Secure Access System v1.0 ] =====");
-    puts("/***************************************\\");
-    puts("| You must login to access this system. |");
-    puts("\**************************************/");
-    printf("--[ Username: ");
-    fgets(&var_78, 0x64, __bss_start);
-    var_78[strcspn(&var_78, "\n")] = 0;
-    printf("--[ Password: ");
-    fgets(&buf, 0x64, __bss_start);
-    buf[strcspn(&buf, "\n")] = 0;
-    puts("*****************************************");
-    
-    if (!strncmp(&buf_1, &buf, 0x29))
-    {
-        printf("Greetings, %s!\n", &var_78, &var_78);
-        system("/bin/sh");
+    if (eax_2 && !eax_2)
         return 0;
-    }
     
-    printf(&var_78);
-    puts(" does not have access!");
-    exit(1);
-    /* no return */
+    puts("nope, incorrect password...\n");
+    return 1;
 }

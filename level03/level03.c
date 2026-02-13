e1cfe001e42b234513bbec1eeddaefe8e848834c
Elise Colin <ecolin@student.42.fr>
2026-02-13 13:43:39 +0100

    feat: updated level03.c with incrementation to find the correct number to give the executable.

--- level03/level03.c
+++ level03/level03.c
@@ -5,13 +5,27 @@
 int main(int argc, char *argv[])
 {
     char key[] = "Q}|u`sfg~sf{}|a3";
-    for (int j = 1;  j > -18; j--)
+    char res[] = "Congratulations!";
+    char test;
+
+    for (int j = 0;  j < 21; j++)
     {
         printf("%d: ", j);
         for (int i = 0; i < strlen(key); i++)
         {
-            printf("%c ", key[i] ^= j);
+            test = key[i] ^ j;
+            //if (test == res[i])
+            printf("%c", test);
         };
         printf("\n");
     }
+    // printf("%d", 0x1337d00d)
+    #define SRCH            0x1337d00d
+    int i = 0;
+    u_int32_t res2 = SRCH;
+    while ((SRCH - res2) !=  0x0000012)
+    {
+        res2++;
+    }
+    printf("%x", res2);
 }
